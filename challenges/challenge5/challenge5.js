a087bcc9ed896f7b3bde133a7867982e3aee805b
Nez Mustafa <nezmustafa123@gmail.com>
2021-01-08 22:30:31 +0000

    challenge comparing average scores using two functions completed

--- challenges/challenge5/challenge5.js
+++ challenges/challenge5/challenge5.js
@@ -17,3 +17,19 @@ const calcAvg = (a, b, c) => {
     return (a + b + c) /3;
 };
 
+const avgDolphins = calcAvg(44, 23, 71);
+const avgKoalas = calcAvg(65, 54, 49);
+
+console.log(avgDolphins);
+console.log(avgKoalas);
+
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >=2 * avgKoalas) {
+        console.log(`Dolphins win ${avgDolphins} to ${avgKoalas}`);
+    } else if (avgKoalas >=2 * avgDolphins) {
+        console.log(`Koalas win ${avgKoalas} to ${avgDolphins}`);
+    }
+};
+
+checkWinner(avgDolphins, avgKoalas);
