de84ef750daf8fb408432010a464f05a14ebd42f
Nez Mustafa <nezmustafa123@gmail.com>
2021-01-12 13:30:11 +0000

    testing indexOf and includes methods

--- challenges/challenge6/challenge6.js
+++ challenges/challenge6/challenge6.js
@@ -38,3 +38,76 @@ const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2]), calcAge(y
 
 
 console.log(ages);
+
+
+//array methods
+
+//ADD ELEMENTS
+
+//push is a function
+
+//add to end of array
+// also returns length of new array
+
+const friendsNew = ['Michael', 'Steven', 'Peter'];
+//call function attached to friends array
+
+//returns length of new array
+
+
+friendsNew.push('Jay');
+console.log(friendsNew);
+
+const arrLength = friends.push('Jay');
+
+console.log(arrLength);
+
+//unshift adds to the front of the array
+//returns lenth of array too
+friendsNew.unshift('John');
+
+
+console.log(friendsNew);
+
+
+//REMOVE ELEMENTS
+
+//pop doesn't return length just element
+
+friendsNew.pop();
+const popped = friendsNew.pop();
+
+//doesn't take in andy parameters
+
+console.log(friendsNew);
+
+console.log(popped);
+
+//remove firrst 
+//returns element that was removed
+friendsNew.shift(); //first
+
+const shited = friendsNew.shift(); //first
+
+//position of element
+
+console.log(friendsNew.indexOf('Steven'));
+
+//steven is index one so this should return one
+
+//for a non existent index it will return minus one
+
+console.log(friendsNew.indexOf('Bob'));
+
+//-1
+
+//includes method returns true if it exists faslse if it doesn't
+//uses strict equality
+console.log(friendsNew.indexOf('Steven'));
+console.log(friendsNew.indexOf('Bob'));
+
+//if array includes peter it's executed
+
+if (friends.includes('Peter')) {
+    console.log(`You hsve s friend called Peter`);
+}
