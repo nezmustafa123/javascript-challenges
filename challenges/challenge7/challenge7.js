e66d3f160a9bc4dc81df395c3601b8338a0b1048
Nez Mustafa <nezmustafa123@gmail.com>
2021-01-14 14:43:18 +0000

    refine object literal

---
+++ challenges/challenge7/challenge7.js
@@ -0,0 +1,15 @@
+//objects
+
+//give elements a name as opposed to an array
+
+//key/property value pairs
+const jonasArray = {
+   name: 'Jonas',
+    surname: 'Schmedtann',
+    age: 2037- 1991,
+     job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+
+//many ways of creating objects
