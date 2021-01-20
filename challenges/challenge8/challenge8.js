702b40a45cac3e833ce761b94486ac835446ebee
Nez Mustafa <nezmustafa123@gmail.com>
2021-01-20 21:44:24 +0000

    playing with for loops

---
+++ challenges/challenge8/challenge8.js
@@ -0,0 +1,22 @@
+//for loop 
+//control structure
+//repeat the sme thing over and over again
+
+console.log('Lifting weights rep 1');
+console.log('Lifting weights rep 2');
+console.log('Lifting weights rep 3');
+console.log('Lifting weights rep 4');
+console.log('Lifting weights rep 5');
+console.log('Lifting weights rep 6');
+console.log('Lifting weights rep 7');
+console.log('Lifting weights rep 8');
+
+//three parts of for statement
+//initialisation, condition, incrementation
+//condition will be incremented before each iteration of loop
+
+//for loop keeps running while condition is true
+//each iteration rep counter will increase
+for(let rep = 1; rep <= 10; rep ++) {
+     
+}
