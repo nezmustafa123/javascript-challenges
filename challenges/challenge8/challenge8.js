a3ba968d5f3aab59b3a6635e95aab0bddc809e3d
Nez Mustafa <nezmustafa123@gmail.com>
2021-01-21 21:37:58 +0000

    using for loop and template literal to print value of counter

--- challenges/challenge8/challenge8.js
+++ challenges/challenge8/challenge8.js
@@ -12,11 +12,15 @@ console.log('Lifting weights rep 7');
 console.log('Lifting weights rep 8');
 
 //three parts of for statement
+//for loop has a counter
+//initialise counter with let
 //initialisation, condition, incrementation
-//condition will be incremented before each iteration of loop
+//counter will be incremented before each iteration of loop
 
 //for loop keeps running while condition is true
 //each iteration rep counter will increase
+//when condition is false loop will stop
+//use rep variable
 for(let rep = 1; rep <= 10; rep ++) {
-     
+     console.log(`Lifting weights rep ${rep}`)
 }
