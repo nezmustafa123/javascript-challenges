1970647fc73c65cd5951edb52c22fd45f2f14cf4
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-02 21:46:39 +0000

    intro to debugging

---
+++ challenges/debugging_skills/script.js
@@ -0,0 +1,21 @@
+!   //debugging skills
+    
+    //IDENTIFY
+    //be aware there is a bug
+    //during development
+    //testing software
+    //user reports during production
+    //idendify the contect i.e broswers
+    
+    
+    //FIND
+    //isolate where the bug is happening
+    //developer console for small bugs
+    //debugger for larger ones
+    
+    //FIX
+    //replace wrong solution with correct solution
+    
+    //PREVENT 
+    //using testing software
+    //search for same bug in similar code
