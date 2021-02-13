1d1f917e6751ac34a623297651bca92fe04e1b06
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-13 21:01:15 +0000

    freecodecamp arguments optional algorithm updated

--- algos/Algo18/algo18.js
+++ algos/Algo18/algo18.js
@@ -1,19 +1,47 @@
-function addTogether(a,b) {
+//function addTogether(a,b) {
+//
+//  
+//if(!Number.isInteger(a) || !Number.isInteger(b)) {
+//return undefined;
+//}
+//
+//  if(!b){
+//    let sumTwoAnd;
+//    sumTwoAnd =  function (a){
+//      return a + b;
+//  }
+//   return sumTwoAnd;
+//   
+//  } else {
+//     return a + b;
+//  }
+//};
+//
+//console.log(addTogether(2)(3));
 
+
+
+
+function addTogether(a,b) {
   
+  if(b === undefined){
+   if(!Number.isInteger(a)) {
+        return undefined;
+     }
+   return function(x){
+     if(!Number.isInteger(x)) {
+        return undefined;
+     }
+      return a + x;
+  }
+
+  } else {
+
 if(!Number.isInteger(a) || !Number.isInteger(b)) {
 return undefined;
 }
 
-  if(!b){
-    let sumTwoAnd;
-    sumTwoAnd =  function (a){
-      return a + b;
-  }
-   return sumTwoAnd;
-   
-  } else {
-     return a + b;
+    return a + b;
   }
 };
 
