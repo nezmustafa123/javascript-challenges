f87349ca0b685db29ea23ea36b78ca1b3ef05e90
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-17 11:26:00 +0000

    freecodecamp map the debris algorithm

---
+++ algos/Algo21/aglo21.js
@@ -0,0 +1,10 @@
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr.map(item => ({
+    name: item.name,
+    orbitalPeriod:(2 * Math.PI) * Math.sqrt(((Math.pow(earthRadius + item.avgAlt), 3) / GM))
+  }));
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
