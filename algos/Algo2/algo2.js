68f1db5b952f3fa5264759e510d9b8e507d613eb
Nez Mustafa <nezmustafa123@gmail.com>
2021-02-18 20:45:14 +0000

    freecodecamp diff two arrays algorithm updated

--- algos/Algo2/algo2.js
+++ algos/Algo2/algo2.js
@@ -1,26 +1,47 @@
 function diffArray(arr1, arr2) {
     var newArr = [];
-    let arrOneLength = arr1.length;
-    let arrTwoLength = arr2.length;
-    let biggerArrayLength = arrOneLength > arrTwoLength ? arrOneLength :    arrTwoLength;
-  
-    let smallerArrayLength = arrOneLength < arrTwoLength? arrOneLength : arrTwoLength;
-  
-    let biggerArray = arrOneLength > arrTwoLength ? arr1 : arr2;
-  
-    let smallerArray = arrOneLength < arrTwoLength ? arr1 : arr2;
-   
-  
-  //loop through bigger array first 
-  //to loop through all elements and not miss any
-  
-    for(let i = 0; i < biggerArrayLength; i ++) {
-      if(!smallerArray.includes(biggerArray[i])){
-          newArr.push(smallerArray[i]);
-        }
-      }
+//    let arrOneLength = arr1.length;
+//    let arrTwoLength = arr2.length;
+//    let biggerArrayLength = arrOneLength > arrTwoLength ? arrOneLength :    arrTwoLength;
+//  
+//    let smallerArrayLength = arrOneLength < arrTwoLength? arrOneLength : arrTwoLength;
+//  
+//    let biggerArray = arrOneLength > arrTwoLength ? arr1 : arr2;
+//  
+//    let smallerArray = arrOneLength < arrTwoLength ? arr1 : arr2;
+//   
+//  
+//  //loop through bigger array first 
+//  //to loop through all elements and not miss any
+//  
+//    for(let i = 0; i < biggerArrayLength; i ++) {
+//      if(!smallerArray.includes(biggerArray[i])){
+//          newArr.push(smallerArray[i]);
+//        }
+//      }
+    
+    arr1.forEach(item => {
+   if(arr2.indexOf(item) === -1) {
+    newArr.push(item);
+   }
+ });
+
+ //one array is bigger so loop through secod one
+
+ arr2.forEach(item => {
+   if(arr1.indexOf(item) === -1){
+     newArr.push(item)
+   }
+ });
+    
     console.log(newArr);
     return newArr;
   }
   
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+
+
+
+
