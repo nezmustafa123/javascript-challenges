2782004f30e1423c2b73910b2796a08818b84965
Nez Mustafa <nezmustafa123@gmail.com>
2021-03-25 15:45:27 +0000

    destructuring continued

--- challenges/es6_skills/script.js
+++ challenges/es6_skills/script.js
@@ -7,8 +7,14 @@ const restaurant = {
     name: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    stargerMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    //two parameters one index for starter menu one index for main menu person will order giving index for each menu
+    
+    order: function(starterIndex, mainIndex) {
+        //starter menu at position starter index main menu at position mainindex
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
 };
 
 //unpacking values from an array or object into seperate variables 
@@ -44,7 +50,62 @@ console.log(firstagain, secondagain);
 
 
 
-//can use to swtich variables say restaruant wanted to switch main and secondary first and second
+//can use to swtich variables say restaruant wanted to switch main and secondary categories
 
-const [main, , secondary] = restaurant.categories;
+let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
+
+//without destructuring 
+//would do something like this 
+//create temporary variable
+const temp = main;
+//italian
+main = secondary;
+//italian becomes vegetarian
+secondary = temp;
+//vegetarian becomes italian
+
+console.log(main, secondary);
+
+
+//use destructuring new array two variables inverted
+//don't need let or const
+[main, secondary] = [secondary,main];
+
+//function returning array then immdietely destruct array into variables
+//recieve array and destructure
+
+console.log(restaurant.order(2,0));
+const [starter, maincourse] = restaurant.order(2,0);
+console.log(starter, mainCourse);
+//nested arrays
+const nested = [2, 4, [5, 6]];
+const [i, ,j] = nested;
+console.log(i, j);
+
+//get all values destructuring within destructuring
+
+const [i, ,[j, k]] = nested;
+
+console.log(i, j, k);
+
+
+//default values
+//const [p, q, r] = [8,9];
+//will get undefined as third one array shorter than you think
+//set default values instead
+
+
+const [p = 1, q = 1, r = 1] = [8,9];
+console.log(p, q, r);
+//r will become one
+
+
+
+
+
+
+
+
+
+
