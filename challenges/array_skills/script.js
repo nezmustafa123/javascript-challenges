f0aceb06a7091d82863f6564414c0031d819231b
Nez Mustafa <nezmustafa123@gmail.com>
2021-05-25 13:14:24 +0100

    array methods

--- challenges/array_skills/script.js
+++ challenges/array_skills/script.js
@@ -6,12 +6,12 @@
 
 
 
-
+//SPLICE
 let arr = ['a', 'b', 'c', 'd', 'e'];
 //slice method similiar to string slice method
 
 arr.slice(2);
-//start at index all the way to end end, returns new array with extracted parts
+//start at index all the way to end end, returns new array with extracted parts doesn't change original 
 
 //define end parameter
 
@@ -31,23 +31,30 @@ arr.slice(-1);
 arr.slice(-1);
 
 arr.slice();
-//creates shallow copy of original array
+//creates shallow copy of original array use slice method
+//use it when chaining multiple methods together
+//like doing 
+[...arr];
 
 
 
+//SPLICE
+//similar to slice but mutates original array
 
+console.log(arr.splice(2));
+//splice deletes from index two in original array and returns the rest
+//the original loses the deleted part
+//all we are interested in it do delete elements using splice
 
+//remove last element in array
 
+arr.splice(-1);
 
+console.log(arr);
+//original element apart from last 
 
+arr.splice(1,2);
+//start at index one and delete two elements
 
-
-
-
-
-
-
-
-
-
+//second parameter is delete count not end parameter
 
