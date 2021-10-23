ce789101939c562cedfc366c301e4f1fb307da22
Nez Mustafa <nezmustafa123@gmail.com>
2021-10-23 12:47:41 +0100

    asynchronous code examples

--- challenges/async/script.js
+++ challenges/async/script.js
@@ -17,3 +17,18 @@ setTimeout(function () {
 p.style.color = "red";
 // async code executed after task that runs in background finishes
 // 'non-blocking'
+
+//callback doesn't make code asynchronous only certain functions like timeout make code asynchronous
+
+const img = document.querySelector(".dog");
+img.src = "dog.jpg";
+img.addEventListener("load", function () {
+  img.classList.add("fadeIn");
+});
+
+p.style.width = "300px";
+//src attribute set asynchronously
+//image will be faded in once the image is completely loaded and load event is emitted
+//once image is loaded it's displayed load event is emitted and callback function called
+//event listeners to not make code asynchronous
+//IMAGE LOADS ASYNCHRONOUSLY IN BACKGROUD
