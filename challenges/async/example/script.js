d87f0791befb390fa1b75fd3c112cfcfb04a50d4
Nez Mustafa <nezmustafa123@gmail.com>
2021-10-26 16:08:45 +0100

    nested xml callback in another one

--- challenges/async/example/script.js
+++ challenges/async/example/script.js
@@ -52,8 +52,9 @@ const countriesContainer = document.querySelector(".countries");
 
 //CALLBACK HELL
 
-const renderCountry = function (data) {
-  const html = `<article class="country">
+const renderCountry = function (data, className = "") {
+  //once neighbouring country arrives will use this
+  const html = `<article class="country ${className}">
   <img class="country__img" src="${data.flag}"/>
   <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
@@ -72,7 +73,10 @@ const renderCountry = function (data) {
 
 //xml requests old scool of doing ajax calls
 const getCountryAndNeighbour = function (country) {
+  //data of neighbouring country result of first call
   //silo code in function
+
+  //AJAX call country (1)
   const request = new XMLHttpRequest(); //create new hhtp request object put it into variable
   request.open("GET", `https://restcountries.com/v2/name/${country}`); //open request
   request.send(); //send request ajax call in background async can't set result into some variable because result not there yet
@@ -80,10 +84,34 @@ const getCountryAndNeighbour = function (country) {
   //the rest of the code keeps runniung while the api is contacted need callback function
   //callback on request object for load event once data arrives callback will be called
   request.addEventListener("load", function () {
+    //callback
     //console.log(this.responseText); //this is request response is in response text
     const [data] = JSON.parse(this.responseText); //convert it back to javascript object, as it's json (big string of text) destructure the big array
     console.log(data); //array containing one object
+    //destructure the array recieved remove the brackets
+    //render country 1
     renderCountry(data);
+
+    //get neighbour country (2)
+    const [neighbour] = data.borders;
+    console.log(neighbour);
+    //if neighbour doesn't exist return immedietely
+    if (!neighbour) return;
+
+    //if it does exist
+    //AJAX call country (2)
+    const request2 = new XMLHttpRequest(); //second ajax call in first one dependant on first one
+    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    //second ajax call then load listener on the request
+    request2.addEventListener("load", function () {
+      //appears after the UK
+      // console.log(request2.responseText);
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2); //not an array anymore just one unique result country codes unique just one result
+      //call the rendercountry method at the end of the inner callback function
+      renderCountry(data2, "neighbour"); //when it is a neighbour pass in neighbouring country
+    });
   });
 };
 //can also do
