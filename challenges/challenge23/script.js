afb4b5d6e4ddb37f4f7b1462a5abc5e276ebba35
Nez Mustafa <nezmustafa123@gmail.com>
2021-11-01 15:24:02 +0000

    challenge23 using multiple apis in sequence

--- challenges/challenge23/script.js
+++ challenges/challenge23/script.js
@@ -1,11 +1,59 @@
 "use strict";
 
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+// const renderError = function (msg) {
+//   //export rendering error
+//   //insert adjacent text
+//   countriesContainer.insertAdjacentText("beforeend", msg);
+//   countriesContainer.style.opacity = 1;
+// };
+const renderCountry = function (data, className = "") {
+  //dedault class name
+  //once neighbouring country arrives will use this
+  const html = `<article class="country ${className}">
+  <img class="country__img" src="${data.flag}"/>
+  <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} million</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+  </div>
+</article>
+`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1; //turn container opacity to one
+};
+
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then((res) => res.json())
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) throw new Error(`problem with geocoding ${res.status}`);
+      return response.json();
+    })
     .then((data) => {
       //gets data which is resolved value
       console.log(data);
-    });
+      console.log(`You are in ${data.city}, ${data.country}`);
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then((response) => {
+      if (!response.ok) throw new error(`Country not (${response.status})`);
+      return response.json();
+    })
+    .then((data) => {
+      renderCountry(data[0]);
+    })
+    .catch((err) => console.error(`${err.message} X_X`));
 };
-whereAmI(52.508, 13, 381);
+// .catch((err) => {
+//   console.log(err);
+// });
+
+whereAmI(51.507351, 0.007758);
+whereAmI(48.8566, 2.3522);
