06d35fe169a514fcdd4fe4c4351852ab3a24cc3d
Nez Mustafa <nezmustafa123@gmail.com>
2021-11-04 15:12:01 +0000

    building and comsunming promises that are encapsulated within asynchronous settimeout functions

--- challenges/async/script.js
+++ challenges/async/script.js
@@ -40,21 +40,21 @@
 //eg dom api geolocation api self contained pieces of software allowing other software to interact class api also
 //online api recieves requests for data and sends it back to client as response
 
-console.log("test start");
+// console.log("test start");
 
-setTimeout(() => console.log("0 sec timer"), 0);
-//will call this timer function after 0 seconds
+// setTimeout(() => console.log("0 sec timer"), 0);
+// //will call this timer function after 0 seconds
 
-//promise that resolves immedietely using promise resolve
-//callback gets called with resolved value returned from promise
-Promise.resolve("Resolved promise 1").then((res) => console.log(res));
-//promise.resolve resolves immeditely
-//simulate promise taking a long time give callback function a heavy task
-Promise.resolve("Resolved promise 2").then((res) => {
-  for (let i = 0; i < 10000000000; i++); //large loop
-  console.log(res); //microtask it contains in the queue takes a long time, timeout callback takes more than 0 seconds now
-});
-console.log("Test end");
+// //promise that resolves immedietely using promise resolve
+// //callback gets called with resolved value returned from promise
+// Promise.resolve("Resolved promise 1").then((res) => console.log(res));
+// //promise.resolve resolves immeditely
+// //simulate promise taking a long time give callback function a heavy task
+// Promise.resolve("Resolved promise 2").then((res) => {
+//   for (let i = 0; i < 10000000000; i++); //large loop
+//   console.log(res); //microtask it contains in the queue takes a long time, timeout callback takes more than 0 seconds now
+// });
+// console.log("Test end");
 //two console logs in the top level code in gloval execution context will run first first two logs will come from synchronous logs
 
 //timers callback put into callback queue first callback of resolved promise put into microtasks queue
@@ -63,22 +63,54 @@ console.log("Test end");
 
 //Building promises
 //promise constructor function takes in one executor function promise constructor will execute the function
+//creates new promise that gets stored in variable promise runs immediately
 
-const lotteryPromise = new Promise(function (resolve, reject) {
-  //function will contain async behaviour trying to handle with promise will return resolved value
-  if (Math.random() >= 0.5) {
-    //random number between 0-1
-    //simulate winning lottery 50% of the time
-    //in this situation the lottery is won
-    //fulfilled promise call the resolve method
-    resolve(`Congrats you WIN`); //fullfilled value will get consumed with then method whatever passed in is r
-  } else {
-    //mark promise being rejected pass in error message handled in catch method
-    reject(`You lost better luck next time`); //message will be available in the catch method
-  }
-}); //promise needs to end up in one of the two states
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   //function will contain async behaviour trying to handle with promise will return resolved value
 
-//consume promise that built
+//   setTimeout(function () {
+//     console.log(`lottery draw taking place`);
+//     if (Math.random() >= 0.5) {
+//       //mark promise as either resolved or rejected
+//       //random number between 0-1
+//       //simulate winning lottery 50% of the time
+//       //in this situation the lottery is won
+//       //mark it as fulfilled/resolved promise call the resolve function
+//       resolve(`Congrats you WIN`); //fullfilled value will get consumed with then method whatever passed in is r
+//     } else {
+//       //mark promise being rejected pass in error message handled in catch method
+//       reject(new Error(`You lost better luck next time`)); //message will be available in the catch method
+//     } //new error object
+//   }, 10000); //promise needs to end up in one of the two states
+// });
 
-lotteryPromise.then((res) => console.log(res)).catch((err) => console.err(err));
-//promise object
+// //consume promise that built
+
+// lotteryPromise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
+// promise object
+// res will be string passed into resolve
+// err will be strong passed into reject function
+
+//promisifying setTimeout function
+// const wait = function (seconds) {
+//   //timer won't fail reject not required
+//   //creating a function returning a promise
+//   return new Promise(function (resolve) {
+//     //no resolved value needed to be returned
+//     setTimeout(resolve, seconds * 1000); //function to be called is resolve function
+//   });
+// };
+
+// wait(6)
+//   .then(() => {
+//     console.log("I waited 6 seconds");
+//     return wait(2); //return new promise inside the then method like before
+//   })
+//   .then(() => console.log("I waited for 1 second"));
+
+//promise gets resolved immedietley static method on promise constructor
+//pass in resolve value
+Promise.resolve("testing123").then((x) => console.log(x));
+Promise.reject(new Error("Problem!")).catch((x) => console.error(x));
