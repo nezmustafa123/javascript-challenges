f11746865322b161346423c78ba03f327660bda7
Nez Mustafa <nezmustafa123@gmail.com>
2021-11-08 21:58:38 +0000

    error handling with try catch

--- challenges/async_await/script.js
+++ challenges/async_await/script.js
@@ -12,6 +12,13 @@ const getPosition = function () {
   });
 };
 
+const renderError = function (msg) {
+  //export rendering error
+  //insert adjacent text
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
 const renderCountry = function (data, className = "") {
   //dedault class name
   //once neighbouring country arrives will use this
@@ -33,39 +40,62 @@ const renderCountry = function (data, className = "") {
 };
 
 const whereAmI = async function () {
-  //use geolocation api promise  getposition to get coords
-  const pos = await getPosition(); //await promise returned from get position
-  console.log(pos);
-  const { latitude: lat, longitude: lng } = pos.coords;
-  //plug lat and lng into geocode api
+  try {
+    //use geolocation api promise  getposition to get coords
+    const pos = await getPosition(); //await promise returned from get position
+    // console.log(pos); //automatically gets rejected
+    const { latitude: lat, longitude: lng } = pos.coords;
+    //plug lat and lng into geocode api
 
-  //reverse geocoding
-  const resGeoCode = await fetch(
-    //returned promise stored in resGeoCode
-    `https://geocode.xyz/${lat},${lng}?geoit=json`
-  );
-  console.log(resGeoCode);
+    //reverse geocoding
+    const resGeoCode = await fetch(
+      //response will have ok property
+      //returned promise stored in resGeoCode
+      `https://geocode.xyz/${lat},${lng}?geoit=json` //promise gets rejected only if no internet connection
+    );
+    if (!resGeoCode.ok) throw new Error(`problem getting location data`);
+    console.log(resGeoCode);
 
-  const dataGeo = await resGeoCode.json(); //also a promise
-  console.log(dataGeo);
-  //returns object with properties
-  //one or more await statements inside the function
-  //await the result of the fetch api promise will stop the execution of the code until the fetch promise is fulfilled
-  //same as
-  //fetch(`https://restcountries.eu/v2/name/${country}`).then((res) =>
-  //   console.log(res)
-  // );
+    const dataGeo = await resGeoCode.json(); //also a promise
+    console.log(dataGeo);
+    //returns object with properties
+    //one or more await statements inside the function
+    //await the result of the fetch api promise will stop the execution of the code until the fetch promise is fulfilled
+    //same as
+    //fetch(`https://restcountries.eu/v2/name/${country}`).then((res) =>
+    //   console.log(res)
+    // );
 
-  const countryRes = await fetch(
-    `https://restcountries.com/v2/name/${dataGeo.country}`
-  ); //will be resolved value of promise
-  //   console.log(res);
-  const countryData = await countryRes.json();
-  //store fullfilled promise data into data variable
-  //   console.log(data);
-  renderCountry(countryData[0]);
+    const countryRes = await fetch(
+      `https://restcountries.com/v2/name/${dataGeo.country}`
+    ); //will be resolved value of promise
+    if (!countryRes.ok) throw new Error(`Problem getting country data`);
+    //   console.log(res);
+    const countryData = await countryRes.json();
+    //store fullfilled promise data into data variable
+    //   console.log(data);
+    renderCountry(countryData[0]);
+  } catch (err) {
+    console.error(`${err} ***`);
+    renderError(`something went wrong X_X ${err.message}`);
+  }
 };
 //async function runs ASYNC in the background
 
 whereAmI();
+whereAmI();
+whereAmI();
+
 console.log("first");
+
+//try catch to catch errors using async functions
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   y = 3; //assignment to constant variable
+// } catch (err) {
+//   //no dot before the catch
+//   //has access to whatever error happened in the try block
+//   alert(err.message); //error  object has message property allert appears as a popup rather than in the console alert message
+// }
